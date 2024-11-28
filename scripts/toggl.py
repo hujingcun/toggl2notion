e3f86268bcceec58dce3095669528ce06621fb9a
malinkang <malinkang@adsconflux.com>
2024-11-28 14:33:47 +0800

    add debug

--- scripts/toggl.py
+++ scripts/toggl.py
@@ -22,7 +22,6 @@ def get_time_entries():
     response = requests.get(
         "https://api.track.toggl.com/api/v9/me/time_entries", params=params, auth=auth
     )
-    print(response)
     time_entries = response.json()
     return time_entries
 
@@ -49,11 +48,14 @@ def insert_to_notion():
             .get("end")
         )
     params = {"start_date": start, "end_date": end}
+    print(params)
     response = requests.get(
         "https://api.track.toggl.com/api/v9/me/time_entries", params=params, auth=auth
     )
     if response.ok:
+        print(response.text)
         time_entries = response.json()
+    
         time_entries.sort(key=lambda x: x["start"], reverse=False)
         for task in time_entries:
             if task.get("pid") is not None and task.get("stop") is not None:
