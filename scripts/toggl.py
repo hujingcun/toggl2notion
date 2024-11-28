8cb089133c3946eecc8a625f7038ab2872c95c2c
malinkang <malinkang@adsconflux.com>
2024-11-28 14:30:28 +0800

    add debug

--- scripts/toggl.py
+++ scripts/toggl.py
@@ -22,6 +22,7 @@ def get_time_entries():
     response = requests.get(
         "https://api.track.toggl.com/api/v9/me/time_entries", params=params, auth=auth
     )
+    print(response)
     time_entries = response.json()
     return time_entries
 
